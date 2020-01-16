e1a522901a0ea1b90f806844cab6baa938dc91b1
akioUnity <akioUnity@gmail.com>
2020-01-17 01:10:25 +0700

    add first widget

--- android/app/src/main/kotlin/com/kaybear/helpers/MyWidgetMonthlyProvider.kt
+++ android/app/src/main/kotlin/com/kaybear/helpers/MyWidgetMonthlyProvider.kt
@@ -7,8 +7,10 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.res.Resources
+import android.graphics.Color
 import android.view.View
 import android.widget.RemoteViews
+import androidx.core.app.NotificationCompatExtras
 import com.kaybear.R
 import com.kaybear.activities.SplashActivity
 import com.kaybear.extensions.config
@@ -18,20 +20,32 @@ import com.kaybear.models.DayMonthly
 import com.kaybear.models.Event
 import com.commons.extensions.*
 import org.joda.time.DateTime
+import android.util.Log
 
 class MyWidgetMonthlyProvider : AppWidgetProvider() {
     private val PREV = "prev"
     private val NEXT = "next"
     private val GO_TO_TODAY = "go_to_today"
     private val NEW_EVENT = "new_event"
+    private val Next_Widget = "next_widget"
+    private val First_Widget = "first_widget"
 
     companion object {
-        private var targetDate = DateTime.now().withDayOfMonth(1)
+        private var targetDate = DateTime.now().withDayOfMonth(1).plusYears(10);
     }
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        targetDate = DateTime.now().withDayOfMonth(1).plusYears(10);
         performUpdate(context)
     }
+
+//    override fun onRestored(context: Context,  oldWidgetIds: IntArray, newWidgetIds: IntArray) {
+//        performUpdate(context)
+//    }
+//
+//    override fun onEnabled(context: Context) {
+//        performUpdate(context)
+//    }
 
     private fun performUpdate(context: Context) {
         MonthlyCalendarImpl(monthlyCalendar, context).getMonth(targetDate)
@@ -54,6 +68,7 @@ class MyWidgetMonthlyProvider : AppWidgetProvider() {
             val pendingIntent = PendingIntent.getActivity(context, Integer.parseInt(dayCode.substring(0, 6)), this, 0)
             views.setOnClickPendingIntent(id, pendingIntent)
         }
+
     }
 
     private fun setupDayOpenIntent(context: Context, views: RemoteViews, id: Int, dayCode: String) {
@@ -70,7 +85,15 @@ class MyWidgetMonthlyProvider : AppWidgetProvider() {
             NEXT -> getNextMonth(context)
             GO_TO_TODAY -> goToToday(context)
             NEW_EVENT -> context.launchNewEventIntent()
-            else -> super.onReceive(context, intent)
+            Next_Widget -> getNextWidget(context)
+            First_Widget->{
+                Log.d("Log", "First_widget")
+                targetDate = DateTime.now().withDayOfMonth(1).plusYears(10);
+                performUpdate(context)
+            }
+            else -> {
+                super.onReceive(context, intent)
+            }
         }
     }
 
@@ -82,6 +105,10 @@ class MyWidgetMonthlyProvider : AppWidgetProvider() {
     private fun getNextMonth(context: Context) {
         targetDate = targetDate!!.plusMonths(1)
         MonthlyCalendarImpl(monthlyCalendar, context).getMonth(targetDate!!)
+    }
+
+    private fun getNextWidget(context: Context) {
+        goToToday(context)
     }
 
     private fun goToToday(context: Context) {
@@ -154,7 +181,7 @@ class MyWidgetMonthlyProvider : AppWidgetProvider() {
                 setBackgroundColor(R.id.day_monthly_number_id, textColor)
                 setTextColor(R.id.day_monthly_number_id, textColor.getContrastColor())
             } else {
-                setTextColor(R.id.day_monthly_number_id, textColor)
+                setTextColor(R.id.day_monthly_number_id, Color.parseColor("#FFFFFF"))
             }
         }
         views.addView(id, newRemoteView)
@@ -171,7 +198,7 @@ class MyWidgetMonthlyProvider : AppWidgetProvider() {
                 val views = RemoteViews(context.packageName, R.layout.fragment_month_widget)
                 views.setText(R.id.top_value, month)
 
-                views.setBackgroundColor(R.id.calendar_holder, context.config.widgetBgColor)
+//                views.setBackgroundColor(R.id.calendar_holder, context.config.widgetBgColor)
 
                 views.setTextColor(R.id.top_value, textColor)
                 views.setTextSize(R.id.top_value, largerFontSize)
@@ -188,8 +215,15 @@ class MyWidgetMonthlyProvider : AppWidgetProvider() {
                 bmp = resources.getColoredBitmap(R.drawable.ic_plus_vector, textColor)
                 views.setImageViewBitmap(R.id.top_new_event, bmp)
 
+//                bmp = resources.getColoredBitmap(R.drawable.ic_plus_vector, textColor)
+//                views.setImageViewBitmap(R.id.first_widget_image, bmp)
+
                 val shouldGoToTodayBeVisible = currTargetDate.withTime(0, 0, 0, 0) != DateTime.now().withDayOfMonth(1).withTime(0, 0, 0, 0)
                 views.setVisibleIf(R.id.top_go_to_today, shouldGoToTodayBeVisible)
+
+                val calendarVisible = currTargetDate.year<2029
+                views.setVisibleIf(R.id.calendar_holder, calendarVisible)
+                views.setVisibleIf(R.id.first_widget_image, !calendarVisible)
 
                 updateDayLabels(context, views, resources, textColor)
                 updateDays(context, views, days)
@@ -199,8 +233,16 @@ class MyWidgetMonthlyProvider : AppWidgetProvider() {
                 setupIntent(context, views, GO_TO_TODAY, R.id.top_go_to_today)
                 setupIntent(context, views, NEW_EVENT, R.id.top_new_event)
 
+                setupIntent(context, views, Next_Widget, R.id.first_widget_image)
+
+                setupIntent(context, views, First_Widget, R.id.first_widget_btn)
+
+
                 val monthCode = days.firstOrNull { it.code.substring(6) == "01" }?.code ?: Formatter.getTodayCode()
+                setupIntent(context, views, First_Widget, R.id.top_value)
                 setupAppOpenIntent(context, views, R.id.top_value, monthCode)
+                setupAppOpenIntent(context, views, R.id.open_app_btn, monthCode)
+
 
                 try {
                     appWidgetManager.updateAppWidget(it, views)
@@ -217,7 +259,7 @@ class MyWidgetMonthlyProvider : AppWidgetProvider() {
         val letters = context.resources.getStringArray(R.array.week_day_letters)
         for (i in 0..6) {
             val id = resources.getIdentifier("label_$i", "id", packageName)
-            views.setTextColor(id, textColor)
+            views.setTextColor(id, Color.parseColor("#0000FF"))
             views.setTextSize(id, smallerFontSize)
 
             var index = i
